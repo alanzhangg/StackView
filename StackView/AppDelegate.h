d66d41538c7ca4d0be07511d006dbad7631f14e6
alanzhangg <alanzhangg@icloud.com>
2015-08-21 18:40:34 +0800

    stackView

---
+++ StackView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackView
+//
+//  Created by zhangalan on 8/21/15.
+//  Copyright © 2015 zhangalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
